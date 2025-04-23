4fe3561798ccd8e82c71823c7bdda7a5aa839b13
NOSi / GAI - Gilberto Rodrigues Fernandes <Gilberto.Fernandes@gov.cv>
2025-04-23 10:15:03 -0100

    ajustes em getDepartmentsQueryHandler e em GetUserRolesQueryHandler

--- src/main/java/cv/igrp/platform/access_management/department/application/queries/queries/GetDepartmentsQuery.java
+++ src/main/java/cv/igrp/platform/access_management/department/application/queries/queries/GetDepartmentsQuery.java
@@ -1,11 +1,33 @@
 package cv.igrp.platform.access_management.department.application.queries.queries;
 
 import cv.igrp.framework.core.domain.Query;
-import jakarta.validation.constraints.*;
 
 
 
 public class GetDepartmentsQuery implements Query {
 
+    private Integer applicationId;
+    private String name;
+    private String status;
 
+    public GetDepartmentsQuery() {
+    }
+
+    public GetDepartmentsQuery(Integer applicationId, String name, String status) {
+        this.applicationId = applicationId;
+        this.name = name;
+        this.status = status;
+    }
+
+    public Integer getApplicationId() {
+        return applicationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
--- src/main/java/cv/igrp/platform/access_management/users/application/queries/handlers/GetUserRolesQueryHandler.java
+++ src/main/java/cv/igrp/platform/access_management/users/application/queries/handlers/GetUserRolesQueryHandler.java
@@ -3,15 +3,15 @@ package cv.igrp.platform.access_management.users.application.queries.handlers;
 import cv.igrp.framework.core.domain.QueryHandler;
 import cv.igrp.framework.stereotype.IgrpQueryHandler;
 import cv.igrp.platform.access_management.shared.infrastructure.persistence.IGRPUserRepository;
+import cv.igrp.platform.access_management.role.domain.service.RoleMapper;
 import cv.igrp.platform.access_management.shared.application.dto.RoleDTO;
+import cv.igrp.platform.access_management.shared.domain.models.IGRPUser;
 import cv.igrp.platform.access_management.users.application.queries.queries.GetUserRolesQuery;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import jakarta.persistence.EntityNotFoundException;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,18 +19,22 @@ import java.util.stream.Collectors;
 public class GetUserRolesQueryHandler implements QueryHandler<GetUserRolesQuery, ResponseEntity<List<RoleDTO>>> {
 
     private final IGRPUserRepository userRepository;
-    //private final RoleMapper roleMapper;
+    private final RoleMapper roleMapper;
 
-    public GetUserRolesQueryHandler(IGRPUserRepository userRepository/*, RoleMapper roleMapper*/) {
+    public GetUserRolesQueryHandler(IGRPUserRepository userRepository, RoleMapper roleMapper) {
         this.userRepository = userRepository;
-        //this.roleMapper = roleMapper;
+        this.roleMapper = roleMapper;
     }
 
     @IgrpQueryHandler
     public ResponseEntity<List<RoleDTO>> handle(GetUserRolesQuery query) {
-        /*IGRPUser user = userRepository.findById(query.getUserId())
-                .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + query.getUserId()));
-        List<RoleDTO> result = user.getRoles().stream().map(roleMapper::toDto).collect(Collectors.toList());*/
-        return ResponseEntity.ok(new ArrayList<>());
+        IGRPUser user = userRepository.findById(query.getId())
+                .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + query.getId()));
+
+        List<RoleDTO> result = user.getRoles().stream()
+                .map(roleMapper::mapToDto)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(result);
     }
 }
