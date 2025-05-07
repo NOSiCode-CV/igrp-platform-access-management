b284fab90255da18e2e67faf467989553da6fb8f
bortet <bruno.ortet@gmail.com>
2025-05-07 16:21:48 -0100

    feat: GetRoleByIdQueryHandler javadoc and unit test.

--- src/main/java/cv/igrp/platform/access_management/role/application/queries/handlers/GetRoleByIdQueryHandler.java
+++ src/main/java/cv/igrp/platform/access_management/role/application/queries/handlers/GetRoleByIdQueryHandler.java
@@ -16,6 +16,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Handles the retrieval of a {@link Role} by its identifier.
+ * <p>
+ * This query handler fetches the role from the repository based on the provided ID, ensuring that the role is not
+ * marked as {@link Status#DELETED}. If the role is found, it is mapped to a {@link RoleDTO} and returned in the
+ * response.
+ * </p>
+ *
+ * <p>
+ * In case no role is found with the specified ID, or if the role has been marked as deleted, an
+ * {@link IgrpResponseStatusException} is thrown with a {@link HttpStatus#NOT_FOUND} status.
+ * </p>
+ */
 @Slf4j
 @Service
 public class GetRoleByIdQueryHandler implements QueryHandler<GetRoleByIdQuery, ResponseEntity<RoleDTO>> {
@@ -23,11 +36,28 @@ public class GetRoleByIdQueryHandler implements QueryHandler<GetRoleByIdQuery, R
     private final RoleRepository roleRepository;
     private final RoleMapper roleMapper;
 
+    /**
+     * Constructs a new instance of {@code GetRoleByIdQueryHandler} with the required dependencies.
+     *
+     * @param roleRepository the repository used to access role entities
+     * @param roleMapper the mapper used to convert {@link Role} entities to {@link RoleDTO}
+     */
     public GetRoleByIdQueryHandler(RoleRepository roleRepository, RoleMapper roleMapper) {
         this.roleRepository = roleRepository;
         this.roleMapper = roleMapper;
     }
 
+    /**
+     * Handles the incoming {@link GetRoleByIdQuery} by retrieving the role with the specified ID from the repository.
+     * <ul>
+     *   <li>If the role is found and is not marked as {@link Status#DELETED}, it is returned as a {@link RoleDTO}.</li>
+     *   <li>If the role is not found or is marked as deleted, an exception is thrown with a {@code 404 NOT FOUND} status.</li>
+     * </ul>
+     *
+     * @param query the query containing the ID of the role to retrieve
+     * @return a {@link ResponseEntity} containing the {@link RoleDTO} with status {@link HttpStatus#OK}
+     * @throws IgrpResponseStatusException if the role is not found or is marked as {@code DELETED}
+     */
     @IgrpQueryHandler
     @Transactional(readOnly = true)
     public ResponseEntity<RoleDTO> handle(GetRoleByIdQuery query) {
