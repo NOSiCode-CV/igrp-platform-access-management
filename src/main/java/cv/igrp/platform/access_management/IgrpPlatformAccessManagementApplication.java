53037b2302189b1f58ae8dabf5a5f594e978a0a2
Marcelo.Monteiro <marcelo.monteiro@nosi.cv>
2025-07-18 11:15:02 -0100

    remove JPA annotation from main class

--- src/main/java/cv/igrp/platform/access_management/IgrpPlatformAccessManagementApplication.java
+++ src/main/java/cv/igrp/platform/access_management/IgrpPlatformAccessManagementApplication.java
@@ -8,11 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 
 @SpringBootApplication
-@EnableJpaAuditing(dateTimeProviderRef = "auditDateTimeProvider", auditorAwareRef = "applicationAuditorAware")
 @ComponentScan(basePackages = "cv.igrp")
 public class IgrpPlatformAccessManagementApplication {
 
